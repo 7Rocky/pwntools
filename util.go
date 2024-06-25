9dc53e68e0a3d29c16452c0ffbd3d116c0722cf9
7Rocky <rockygitt@gmail.com>
2024-06-25 11:59:37 +0200

    doc

--- util.go
+++ util.go
@@ -10,6 +10,7 @@ type number interface {
 	int | string
 }
 
+// UnHex hex-decodes a string
 func UnHex(h string) []byte {
 	b, err := hex.DecodeString(h)
 
@@ -20,10 +21,12 @@ func UnHex(h string) []byte {
 	return b
 }
 
+// Hex creates a hex-string from a byte slice
 func Hex(b []byte) string {
 	return hex.EncodeToString(b)
 }
 
+// Xor applies a byte-wise XOR operation to the byte slices passed as arguments
 func Xor(a []byte, b ...[]byte) []byte {
 	length := len(a)
 
@@ -48,6 +51,7 @@ func Xor(a []byte, b ...[]byte) []byte {
 	return r
 }
 
+// B64d Base64-decodes a string
 func B64d(e string) []byte {
 	d, err := base64.StdEncoding.DecodeString(e)
 
@@ -58,32 +62,38 @@ func B64d(e string) []byte {
 	return d
 }
 
+// B64e Base64-encodes a string
 func B64e(d []byte) string {
 	return base64.StdEncoding.EncodeToString(d)
 }
 
+// P64 packs a 64-bit unsigned integer
 func P64(v uint64) []byte {
 	b := make([]byte, 8)
 	binary.LittleEndian.PutUint64(b, v)
 	return b
 }
 
+// P32 packs a 32-bit unsigned integer
 func P32(v uint32) []byte {
 	b := make([]byte, 4)
 	binary.LittleEndian.PutUint32(b, v)
 	return b
 }
 
+// P16 packs a 16-bit unsigned integer
 func P16(v uint16) []byte {
 	b := make([]byte, 2)
 	binary.LittleEndian.PutUint16(b, v)
 	return b
 }
 
+// P8 packs an 8-bit unsigned integer
 func P8(v uint8) []byte {
 	return []byte{v}
 }
 
+// U64 unpacks a 64-bit unsigned integer
 func U64(b []byte) uint64 {
 	if len(b) != 8 {
 		Error("U64 requires a buffer of 8 bytes")
@@ -92,6 +102,7 @@ func U64(b []byte) uint64 {
 	return binary.LittleEndian.Uint64(b)
 }
 
+// U32 unpacks a 32-bit unsigned integer
 func U32(b []byte) uint32 {
 	if len(b) != 4 {
 		Error("U32 requires a buffer of 4 bytes")
@@ -99,6 +110,8 @@ func U32(b []byte) uint32 {
 
 	return binary.LittleEndian.Uint32(b)
 }
+
+// U16 unpacks a 16-bit unsigned integer
 func U16(b []byte) uint16 {
 	if len(b) != 2 {
 		Error("U16 requires a buffer of 2 bytes")
@@ -106,6 +119,8 @@ func U16(b []byte) uint16 {
 
 	return binary.LittleEndian.Uint16(b)
 }
+
+// U8 unpacks an 8-bit unsigned integer
 func U8(b []byte) uint8 {
 	if len(b) != 1 {
 		Error("U8 requires a buffer of 1 byte")
